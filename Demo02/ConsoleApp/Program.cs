5ef59f8832999bfa8977e802cf9c7afc8e7244c7
Daniel Diaz <daaniieeliitoodiiaaz@gmail.com>
2024-12-11 20:56:13 -0500

    Demo02

---
+++ Demo02/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyLibrary;
+
+Employee Maria = new Secretary();
+Maria.Name = "Maria";
+Employee Pedro = new Manager();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer();
+Console.WriteLine($"Dias: {Miguel.GetPaymentDays()}");
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Pedro);
+WriteEmployeeInfo(Miguel);
+
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name}: {employee.CalculateMonthlyPayment()}");
+    Console.WriteLine($"Days: {employee.GetPaymentDays()}");
+}
